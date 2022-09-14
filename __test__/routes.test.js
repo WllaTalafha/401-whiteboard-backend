1fa8e56ed7e58572e36ce29bd527c26f3289a1e6
WllaTalafha <wlla.issa396@gmail.com>
2022-09-14 12:48:37 +0300

    test

--- __test__/routes.test.js
+++ __test__/routes.test.js
@@ -33,8 +33,8 @@ describe('testing the CRUD app', () => {
 
      it('test update one post',async () => {
         const data = {
-            postTitle:"hi",
-            postContent: "lolo",
+            postTitle:"hello lovely",
+            postContent: "wlla",
             postStatus: false
         }
         let res = await req.put('/post/2').send(data);
@@ -44,9 +44,7 @@ describe('testing the CRUD app', () => {
 
 
      it('test delete one post',async () => {
-        let res = await req.delete('/post/8');
+        let res = await req.delete('/post/6');
         expect(res.statusCode).toBe(204);
      });
-
-
 });
