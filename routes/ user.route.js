db776bf3692aa812e9ac1a0e17ca0470ff9d89b6
WllaTalafha <wlla.issa396@gmail.com>
2022-09-19 14:26:56 +0300

    auth

--- routes/ user.route.js
+++ routes/ user.route.js
@@ -2,10 +2,10 @@
 const express = require('express');
 const router = express.Router();
 const { signUp , login } = require( '../controllers/user.controller' );
-// const {saveUser} = require( '../middlewares/userAuth' );
+const {saveUser} = require( '../middlewares/userAuth' );
 
 
-router.post( '/signup', signUp );
+router.post( '/signup',saveUser, signUp );
 router.post( '/signin', login );
 
 module.exports = router;
